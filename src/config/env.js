a4bd8f1124e34575c7ee28272241f6ba7986e259
Nicolas Frugoni <nfrugoni@colven.com.ar>
2026-05-13 11:06:30 -0300

    subir comprobacion capas

---
+++ src/config/env.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log('✅ Variables de entorno cargadas')
+
+export const env = {
+
+  PORT: process.env.PORT,
+
+  MONGO_URI: process.env.MONGO_URI
+
+}
